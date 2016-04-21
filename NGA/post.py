bef353427189c4fcff82d0f7408e0afecec68f38
icyblade <icyblade.py@gmail.com>
2016-04-21 13:48:03 +0800

    新增加分功能

--- NGA/post.py
+++ NGA/post.py
@@ -1,5 +1,5 @@
 #! coding: utf8
-import json, time, re
+import json, time, re, sys
 from operator import itemgetter
 from datetime import datetime, timedelta
 from lib.parallel_urllib import ParallelUrllib
@@ -22,3 +22,19 @@ class Post:
                 yield int(content['pid']), int(content['postdatetimestamp']), int(content['authorid'])
         else:
             yield 0, 0, 0
+
+    def add_point(self, value, info, tid, pm=True, addmoney=True, norvrc=False):
+        """加分"""
+        url = 'http://bbs.ngacn.cc/nuke.php?func=addpoint&tid={tid}&pid={self.pid}'.format(
+            self = self, tid = tid
+        )
+        post_data = {
+            'rcvc': value,
+            'info': info,
+            'pm'  : 1 if pm else 0,
+            'addmoney': 1 if addmoney else 0,
+            'norvrc': 1 if norvrc else 0,
+        }
+        #sys.stdout.write(str(url)+'\n')
+        self.opener.post_once(url, post_data) # todo post 会重复加分，solve it
+            
