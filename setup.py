2e33adc4551f6594bbf930bc17a837cc4748f291
icyblade <icyblade.py@gmail.com>
2017-09-25 09:30:18 +0800

    add travis and ready for pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='pynga',
-    version='2.0',
+    version='2.0.0',
     packages=['pynga'],
     url='https://github.com/icyblade/pynga',
     license='MIT',
     author='Icyblade Dai',
-    author_email='',
-    description='Python implementation of bbs.ngacn.cc'
+    author_email='icyblade.aspx@gmail.com',
+    description='Python implementation of bbs.ngacn.cc',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+    python_requires='>=3.6',
 )
